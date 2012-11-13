0144f26da8f7f7842fdae316cadc8e24d3a628ce
Rishabh Mahajan <rishabhmhjn@gmail.com>
2012-11-14 01:38:14 +0900

    solr document library almost completed. the remaining part will be done as soon as the front starts to work
    Signed-off-by: Rishabh Mahajan <rishabhmhjn@gmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 /**
  * @author rishabhmhjn
- *
- *
  */
+
+var nodejs_solr = module.exports = require('./lib'), logger = require('nlogger')
+    .logger(module);
+
+var doc = new nodejs_solr.document();
+
+doc.setMultiValue("name", "Rishabh Mahajan");
+doc.setMultiValue("name", "Tushar Mahajan");
+
+logger.debug(doc);
+
+doc.clear();
+
+logger.debug(doc);
